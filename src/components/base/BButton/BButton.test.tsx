4c4a9eb108c011bf27705ef85b52b5ea372c7794
Полторацкий Владимир <poltorackii@firecode.ru>
2023-02-12 15:35:01 +0300

    returned SSR, developed BButton

--- src/components/base/BButton/BButton.test.tsx
+++ src/components/base/BButton/BButton.test.tsx
@@ -13,14 +13,14 @@ describe('BButton', () =>{
 
   test('makes button tag disabled if passed disabled prop', () => {
 
-    render(<BButton disabled >button text</BButton>);
+    render(<BButton disabled>button text</BButton>);
     const button = screen.getByText(/button text/i);
     // @ts-ignore
     expect(button.disabled).toBe(true);
   });
 
   test('adds disabled class if disabled', () => {
-    render(<BButton disabled >button text</BButton>);
+    render(<BButton disabled>button text</BButton>);
     const linkElement = screen.getByText(/button text/i);
     expect(linkElement.className).toContain('bbutton--disabled');
   });
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/main.css';
 
 function MyApp({ Component, pageProps }) {
-    return  <SafeHydrate><Component {...pageProps} /></SafeHydrate>
+    return  <Component {...pageProps} />
 }
 
 export default MyApp;
